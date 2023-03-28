ed685c6c6bada2ea79ae562dfc7fe8bf5d83ebe9
avnerjose <avnerj.g.r@gmail.com>
2023-03-28 14:15:19 -0300

    feat(dashboard.tsx): adding avatar to admin/professor profile card

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,10 +1,16 @@
 import { Outlet } from "@remix-run/react";
+import * as Avatar from "@radix-ui/react-avatar";
 
 function Dashboard() {
   return (
     <div className="grid grid-cols-dashboard min-h-screen">
       <aside className="flex flex-col bg-blue-500 drop-shadow-default">
-        <div className="px-6 py-5 border-b border-gray-200">
+        <div className="flex items-center gap-2 px-6 py-5 border-b border-gray-200">
+          <Avatar.Root className="flex items-center justify-center bg-gray-100 w-14 h-14 rounded-full">
+            <Avatar.Fallback>
+              <span className="text-xl font-bold text-blue-900">RM</span>
+            </Avatar.Fallback>
+          </Avatar.Root>
           <div className="flex flex-col">
             <span className="text-xl font-bold text-white">Renzo Mequista</span>
             <span className="text-gray-200">Administrador</span>
