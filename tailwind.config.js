68d3ca70957f208d6b4d98db24e5bf78b20a9ddf
avnerjose <avnerj.g.r@gmail.com>
2023-03-28 13:43:12 -0300

    feat: creating remix project and installing tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.tsx"],
+  theme: {
+    colors: {
+      "blue-500": "#0A5EC1",
+      "blue-900": "#012244",
+      "gray-100": "#F6F6F8",
+      "gray-200": "#CED4DA",
+      "gray-500": "#868E96",
+      "gray-700": "#343A40",
+      "red-500": "#EC3137",
+    },
+    extend: {
+      gridTemplateColumns: {
+        dashboard: "280px 1fr",
+      },
+      dropShadow: {
+        default: "0px 2px 10px rgba(0,0,0,0.25)",
+      },
+    },
+  },
+  plugins: [],
+};
