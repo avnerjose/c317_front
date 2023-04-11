346a318dbeb96e47776cd0004e1708355796919b
avnerjose <avnerj.g.r@gmail.com>
2023-04-10 21:54:38 -0300

    feat(dashboard/disciplines/manage): creating manage disciplines page

---
+++ app/routes/dashboard/disciplines/manage/$code.tsx
@@ -0,0 +1,162 @@
+import type { ErrorBoundaryComponent, LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { DotsThreeCircle, Plus } from "phosphor-react";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const { code } = params;
+  const disciplines = [
+    {
+      name: "Tópicos especiais 1",
+      code: "C317",
+      professor: "Renzo Mesquita",
+    },
+    {
+      name: "Algoritmos 1",
+      code: "C202",
+      professor: "Carlos Ynoguti",
+    },
+  ];
+
+  const discipline = disciplines.find((d) => d.code === code);
+
+  if (!discipline) {
+    throw new Error("Disciplina não encontrada");
+  }
+
+  return json({
+    discipline,
+  });
+};
+
+function ManageDiscipline() {
+  const {
+    discipline: { code, name },
+  } = useLoaderData<typeof loader>();
+
+  return (
+    <>
+      <h2 className="text-xl text-gray-800">
+        Disciplinas /{" "}
+        <Link to="/dashboard/disciplines/manage" className="hover:underline">
+          Gerenciar disciplinas
+        </Link>{" "}
+        / {code}
+      </h2>
+      <h3 className="text-2xl text-gray-800 mt-4">
+        {code} - {name}
+      </h3>
+      <div className="grid grid-cols-3 grid-flow-row gap-6 mt-4 items-start">
+        <div className="bg-white rounded-lg overflow-hidden drop-shadow-md">
+          <div className="flex items-center justify-center p-4 border-b border-b-gray-200 bg-green-500 drop-shadow-md">
+            <span className="text-white font-bold text-xl">Aulas</span>
+          </div>
+          <div className="flex flex-col py-4">
+            <table className="border-separate border-spacing-x-8 text-left">
+              <thead className="text-gray-800">
+                <tr>
+                  <th>Dia da semana</th>
+                  <th>Horário</th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-y00">
+                <tr>
+                  <td>Segunda-feira</td>
+                  <td>10:00</td>
+                </tr>
+                <tr>
+                  <td>Sexta-feira</td>
+                  <td>20:00</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg overflow-hidden drop-shadow-md">
+          <div className="flex items-center justify-center p-4 border-b border-b-gray-200 bg-green-500 drop-shadow-md">
+            <span className="text-white font-bold text-xl">Alunos</span>
+          </div>
+          <div className="flex flex-col py-4">
+            <table className="border-separate border-spacing-x-8 text-left">
+              <thead className="text-gray-800">
+                <tr>
+                  <th>Aluno</th>
+                  <th>Matrícula</th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-y00">
+                <tr>
+                  <td>Fábio Fiorita</td>
+                  <td>24</td>
+                </tr>
+                <tr>
+                  <td>Bezinho Gandolpho</td>
+                  <td>169</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg overflow-hidden drop-shadow-md">
+          <div className="flex items-center justify-center p-4 border-b border-b-gray-200 bg-green-500 drop-shadow-md">
+            <span className="text-white font-bold text-xl">Avaliações</span>
+          </div>
+          <div className="flex flex-col py-4">
+            <table className="border-separate border-spacing-x-8 text-left">
+              <thead className="text-gray-800">
+                <tr>
+                  <th>Código</th>
+                  <th>Data</th>
+                  <th>
+                    <Plus size={24} />
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-y00">
+                <tr>
+                  <td>NP1</td>
+                  <td>24/03/21</td>
+                  <td>
+                    <DotsThreeCircle size={24} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>NP2</td>
+                  <td>13/04/21</td>
+                  <td>
+                    <DotsThreeCircle size={24} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>NP3</td>
+                  <td>26/06/21</td>
+                  <td>
+                    <DotsThreeCircle size={24} />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ManageDiscipline;
+
+export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
+  return (
+    <>
+      <h2 className="text-xl text-gray-800">
+        Disciplinas / Gerenciar disciplinas
+      </h2>
+      <div className="flex flex-col bg-red-200 p-4 rounded-lg mt-4">
+        <span className="text-red-500 text-lg uppercase font-bold">
+          Erro ao gerenciar disciplina
+        </span>
+        <span className="text-red-500">{error.message}</span>
+      </div>
+    </>
+  );
+};
---
+++ app/routes/dashboard/disciplines/manage/index.tsx
@@ -0,0 +1,50 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = () => {
+  return json({
+    disciplines: [
+      {
+        name: "Tópicos especiais 1",
+        code: "C317",
+      },
+      {
+        name: "Algoritmos 1",
+        code: "C202",
+      },
+    ],
+  });
+};
+
+function ManageDisciplines() {
+  const { disciplines } = useLoaderData<typeof loader>();
+
+  return (
+    <>
+      <h2 className="text-xl text-gray-800">
+        Disciplinas / Gerenciar disciplinas
+      </h2>
+      <h1 className="text-2xl mt-4 text-gray-800">Gerenciar disciplinas</h1>
+      <div className="grid grid-cols-4 mt-4 w-full drop-shadow-md gap-6">
+        {disciplines.map((discipline) => (
+          <Link
+            to={`/dashboard/disciplines/manage/${discipline.code}`}
+            className="flex flex-col items-center overflow-hidden text-gray-700 bg-white rounded-lg hover:bg-gray-200 transition-colors group"
+            key={discipline.code}
+          >
+            <div className="bg-green-500 w-full flex items-center justify-center p-4 group-hover:bg-green-700 transition-colors">
+              <span className="text-4xl font-bold text-white">
+                {discipline.code}
+              </span>
+            </div>
+            <div className="p-4">
+              <span>{discipline.name}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default ManageDisciplines;
